7c1525ce292432c8ea7dd3cab10e7866cafed384
Krzysztof Jurewicz <krzysztof.jurewicz@gmail.com>
2014-03-26 19:57:07 +0100

    Initial project code.

---
+++ runtests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from django.conf import settings
+settings.configure(
+    INSTALLED_APPS=(
+        'django.contrib.contenttypes',
+        'terminator',
+    ),
+    DATABASES={
+        'default': {'ENGINE': 'django.db.backends.sqlite3'}
+    })
+
+from django.test.utils import setup_test_environment
+setup_test_environment()
+
+from django.core.management import call_command
+call_command('test', 'terminator')
